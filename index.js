3a04b68fe0f9819911ab6673385f3e31bcec7615
Muhammad Usama Sharif <112772227+musa781@users.noreply.github.com>
2025-04-12 15:02:46 +0500

    Add files via upload
    Project Description: jQuery Interaction Demo
    This is a simple front-end project demonstrating basic jQuery functionality. It includes:
    
    An HTML page styled with a CSS file
    
    jQuery-powered JavaScript to manipulate DOM elements dynamically
    
    Interactive elements like buttons and links
    
    A linked external image (google.png)
    
    Inline jQuery functions for tasks like:
    
    Changing text and attributes
    
    Handling click events to update styles
    
    This setup is ideal for beginners learning how to connect HTML, CSS, and jQuery.

---
+++ index.js
@@ -0,0 +1,9 @@
+//$("h1").text("bye");
+//$("button").text("don't text");
+//console.log($("img").attr("src"));
+//$("a").attr("href", "https://www.yahoo.com");
+//$("button").click(function() {
+   // $("h1").css("color", "red");
+
+//});  //click on hello, the funtio will callback
+
